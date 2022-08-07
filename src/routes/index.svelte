fc93f3bc5fc923c94d140ff12029a12b7f05fc98
Dave Stanton <dstanton@Daves-iMac-2.local>
2022-08-07 08:50:39 -0400

    Adding basic grid and removing header cruft.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
 <h1 class="text-3xl font-bold underline">gotoplanb.xyz</h1>
 <p>&nbsp;</p>
+
+<div class="grid grid-cols-2 md:grid-cols-12">
+	<!-- Banner container -->
+	<div class="col-span-12">
+		<div class="relative">
+			<div class="absolute inset-x-0 bottom-0 h-1/2 bg-gray-100" />
+			<div class="">
+				<div class="relative shadow-xl sm:overflow-hidden">
+					<div class="absolute inset-0">
+						<!-- Banner image -->
+						<img
+							class="h-full w-full object-cover"
+							src="/moneyneversleeps.png"
+							alt="Money Never Sleeps"
+						/>
+					</div>
+					<div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8" />
+				</div>
+			</div>
+		</div>
+	</div>
+	<div>02</div>
+	<div>03</div>
+	<div>04</div>
+	<div>05</div>
+	<div>06</div>
+	<div>07</div>
+	<div>08</div>
+	<div>09</div>
+	<div>10</div>
+	<div>11</div>
+	<div>12</div>
+</div>
