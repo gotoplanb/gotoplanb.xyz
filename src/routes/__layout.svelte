6175ed5c76e4c4f5e5cf6ac00bcd125a21aae84c
Dave Stanton <dstanton@Daves-iMac-2.local>
2022-08-12 08:58:09 -0400

    Bare minimum links.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,20 +6,20 @@
 	<nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
 		<div class="w-full py-4 flex items-center justify-between">
 			<div class="flex items-center">
-				<div class="hidden space-x-8 lg:block text-base font-medium text-slate-600 hover:text-slate-800">
+				<div class="space-x-8 lg:block text-base font-medium text-slate-600 hover:text-slate-800">
 					<a href="/">gotoplanb.xyz</a>
 					<a href="/">Gallery</a>
 					<a href="/collections">Collections</a>
 					<a href="/links">Links</a>
 				</div>
 			</div>
-			<div class="ml-10 space-x-4">
+			<!-- <div class="ml-10 space-x-4">
 				<a
 					href="#"
 					class="inline-block bg-slate-600 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-slate-700"
 					>Connect</a
 				>
-			</div>
+			</div> -->
 		</div>
 	</nav>
 </header>
--- src/routes/links.svelte
+++ src/routes/links.svelte
@@ -1,2 +1,14 @@
-<h1 class="text-3xl font-bold underline">Links</h1>
-<p>&nbsp;</p>
+<div class="grid grid-cols-2 ml-8">
+	<div class="col-span-2">
+		<h1 class="text-3xl font-bold">Official Links</h1>
+		<h2 class="text-xl font-bold">SINKHOLES</h2>
+		<ul>
+			<li><a class="underline" href="https://opensea.io/collection/gotoplanb-sinkholes">Collection on OpenSea</a></li>
+			<li>
+				<a class="underline" href="https://opensea.io/assets/ethereum/0x30d93d1377246e18d929332bd59582f1c316dffe/1"
+					>Utility token on OpenSea</a
+				>
+			</li>
+		</ul>
+	</div>
+</div>
