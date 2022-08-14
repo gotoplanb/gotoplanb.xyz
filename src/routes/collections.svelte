21bb45b144d1ee1e08fbe6e4e446dca773ad7ac6
Dave Stanton <dstanton@Daves-iMac-2.local>
2022-08-14 09:28:01 -0400

    Better avatar code.

--- src/routes/collections.svelte
+++ src/routes/collections.svelte
@@ -1,2 +1,9 @@
-<h1 class="text-3xl font-bold underline">Collections</h1>
-<p>&nbsp;</p>
+<div class="grid grid-cols-2 ml-8">
+	<div class="col-span-2">
+		<h1 class="text-3xl font-bold">Collections</h1>
+		<h2 class="text-xl font-bold">SINKHOLES</h2>
+	</div>
+    <div class="col-span-2">
+		<p>Lorem ipsum</p>
+	</div>
+</div>
