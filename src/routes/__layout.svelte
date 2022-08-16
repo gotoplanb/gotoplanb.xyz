1ef4dd1f000abd2c272522dc470d9152b14aea84
Dave Stanton <davestanton@Daves-MacBook-Pro.local>
2022-08-15 21:10:34 -0400

    Better header for phones.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,24 +2,45 @@
 	import '../app.css';
 </script>
 
-<header class="">
+<!-- This example requires Tailwind CSS v2.0+ -->
+<header class="bg-white">
 	<nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
-		<div class="w-full py-4 flex items-center justify-between">
+		<div
+			class="w-full py-6 flex items-center justify-between border-b border-slate-600 lg:border-none"
+		>
 			<div class="flex items-center">
-				<div class="space-x-8 lg:block text-base font-medium text-slate-600 hover:text-slate-800">
-					<a href="/">gotoplanb.xyz</a>
-					<a href="/">Gallery</a>
-					<a href="/collections">Collections</a>
-					<a href="/links">Links</a>
+				<a href="/">
+					<span>gotoplanb.xyz</span>
+					<!-- TODO Add some logo maybe -->
+					<!-- <span class="sr-only">gotoplanb.xyz</span>
+			<img class="h-10 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=black" alt=""> -->
+				</a>
+				<div class="hidden ml-10 space-x-8 lg:block">
+					<a href="/" class="text-base font-medium text-slate-600 hover:text-slate-800">
+						Gallery
+					</a>
+
+					<a href="/collections" class="text-base font-medium text-slate-600 hover:text-slate-800">
+						Collections
+					</a>
+
+					<a href="/links" class="text-base font-medium text-slate-600 hover:text-slate-800">
+						Links
+					</a>
 				</div>
 			</div>
-			<!-- <div class="ml-10 space-x-4">
-				<a
-					href="#"
-					class="inline-block bg-slate-600 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-slate-700"
-					>Connect</a
-				>
-			</div> -->
+			<div class="ml-10 space-x-4">
+				<!-- <a href="#" class="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75">Connect</a> -->
+			</div>
+		</div>
+		<div class="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+			<a href="/" class="text-base font-medium text-slate-600 hover:text-slate-800"> Gallery </a>
+
+			<a href="/collections" class="text-base font-medium text-slate-600 hover:text-slate-800">
+				Collections
+			</a>
+
+			<a href="/links" class="text-base font-medium text-slate-600 hover:text-slate-800"> Links </a>
 		</div>
 	</nav>
 </header>
