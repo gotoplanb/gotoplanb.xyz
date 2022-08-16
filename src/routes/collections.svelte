fdb83509fc6440da168a3d71f0d0197be158e949
Dave Stanton <davestanton@Daves-MacBook-Pro.local>
2022-08-15 21:51:39 -0400

    wip.

--- src/routes/collections.svelte
+++ src/routes/collections.svelte
@@ -9,6 +9,8 @@
       <p class="mt-6 text-xl text-indigo-100 max-w-3xl">Mattis amet hendrerit dolor, quisque lorem pharetra. Pellentesque lacus nisi urna, arcu sociis eu. Orci vel lectus nisl eget eget ut consectetur. Sit justo viverra non adipisicing elit distinctio.</p>
     </div>
   </div>
+
+  <hr class='mt-4'/>
   
   <!-- This example requires Tailwind CSS v2.0+ -->
 <div class="relative bg-gray-800">
@@ -35,29 +37,56 @@
       </div>
     </div>
   </div>
+
 <hr class='mt-4'/>
-  <!-- This example requires Tailwind CSS v2.0+ -->
-  <div class="relative bg-gray-800">
-    <div class="h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
-      <img class="w-full h-full object-cover" src="./sinkholes-2022-august-devils-millhopper.png" alt="">
-    </div>
-    <div class="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
-      <div class="md:ml-auto md:w-1/2 md:pl-10">
-        <h2 class="text-lg font-semibold text-gray-300">Award winning support</h2>
-        <p class="mt-2 text-white text-3xl font-bold tracking-tight sm:text-4xl sm:tracking-tight">We’re here to help</p>
-        <p class="mt-3 text-lg text-gray-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat quisque ut interdum tincidunt duis.</p>
-        <div class="mt-8">
-          <div class="inline-flex rounded-md shadow">
-            <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50">
-              Visit the help center
-              <!-- Heroicon name: solid/external-link -->
-              <svg class="-mr-1 ml-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
-                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
-              </svg>
-            </a>
-          </div>
+<!-- MARK could work for some list view -->
+
+<!--
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/aspect-ratio'),
+    ],
+  }
+  ```
+-->
+<div class="bg-white">
+    <div class="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+      <div class="space-y-12">
+        <div class="space-y-5 sm:space-y-4 md:max-w-xl lg:max-w-3xl xl:max-w-none">
+          <h2 class="text-3xl font-bold tracking-tight sm:text-4xl sm:tracking-tight">SINKHOLES Editions</h2>
+          <p class="text-xl text-gray-500">Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor ultricies donec risus sodales. Tempus quis et.</p>
         </div>
+        <ul role="list" class="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8">
+          <li>
+            <div class="space-y-4">
+              <div class="aspect-w-3 aspect-h-2">
+                <img class="object-cover shadow-lg rounded-lg" src="./sinkholes-2022-august-devils-millhopper.png" alt="">
+              </div>
+  
+              <div class="space-y-2">
+                <div class="text-lg leading-6 font-medium space-y-1">
+                  <h3>Devil's Millhopper</h3>
+                  <p class="text-indigo-600">Gainesville, FL, USA</p>
+                </div>
+              </div>
+            </div>
+          </li>
+          
+  
+          <!-- More items... -->
+        </ul>
       </div>
     </div>
   </div>
+  
+
+
+  
