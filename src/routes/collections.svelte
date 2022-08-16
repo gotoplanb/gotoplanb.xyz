0476d244fff7e8b95f3cfeadb044ee15a5762b6b
Dave Stanton <davestanton@Daves-MacBook-Pro.local>
2022-08-15 21:25:37 -0400

    WIP collectiosn with some better content areas.

--- src/routes/collections.svelte
+++ src/routes/collections.svelte
@@ -1,31 +1,63 @@
-<script>
-	import VideoPlayer from 'svelte-video-player';
-</script>
-
-<div class="grid grid-cols-2 ml-8">
-	<div class="col-span-2">
-		<h1 class="text-3xl font-bold">Collections</h1>
-		<h2 class="text-xl font-bold">SINKHOLES</h2>
-	</div>
-	<div class="col-span-2">
-		<p>Lorem ipsum</p>
-	</div>
-</div>
-
-<!-- <VideoPlayer
-	poster="./sinkholes-2022-august-devils-millhopper.png"
-	source="./sinkholes-2022-august-devils-millhopper.m4v"
-	loop
-    color='#0021a5'
-/> -->
-
-<div class="grid grid-cols-2 md:grid-cols-12">
-	<div class="col-span-2 md:col-span-6">
-		<VideoPlayer
-			poster="./sinkholes-2022-august-devils-millhopper.png"
-			source="./sinkholes-2022-august-devils-millhopper.m4v"
-			loop
-			color="#0021a5"
-		/>
-	</div>
-</div>
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="relative bg-indigo-800">
+    <div class="absolute inset-0">
+      <img class="w-full h-full object-cover" src="./sinkholes-2022-august-devils-millhopper.png" alt="">
+      <div class="absolute inset-0 bg-indigo-800 mix-blend-multiply" aria-hidden="true"></div>
+    </div>
+    <div class="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
+      <h1 class="text-4xl font-bold tracking-tight text-white sm:text-5xl sm:tracking-tight lg:text-6xl lg:tracking-tight">SINKHOLES</h1>
+      <p class="mt-6 text-xl text-indigo-100 max-w-3xl">Mattis amet hendrerit dolor, quisque lorem pharetra. Pellentesque lacus nisi urna, arcu sociis eu. Orci vel lectus nisl eget eget ut consectetur. Sit justo viverra non adipisicing elit distinctio.</p>
+    </div>
+  </div>
+  
+  <!-- This example requires Tailwind CSS v2.0+ -->
+<div class="relative bg-gray-800">
+    <div class="h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
+      <img class="w-full h-full object-cover" src="./sinkholes-2022-august-devils-millhopper.png" alt="">
+    </div>
+    <div class="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+      <div class="md:ml-auto md:w-1/2 md:pl-10">
+        <h2 class="text-lg font-semibold text-gray-300">Award winning support</h2>
+        <p class="mt-2 text-white text-3xl font-bold tracking-tight sm:text-4xl sm:tracking-tight">We’re here to help</p>
+        <p class="mt-3 text-lg text-gray-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat quisque ut interdum tincidunt duis.</p>
+        <div class="mt-8">
+          <div class="inline-flex rounded-md shadow">
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50">
+              Visit the help center
+              <!-- Heroicon name: solid/external-link -->
+              <svg class="-mr-1 ml-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<hr class='mt-4'/>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <div class="relative bg-gray-800">
+    <div class="h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
+      <img class="w-full h-full object-cover" src="./sinkholes-2022-august-devils-millhopper.png" alt="">
+    </div>
+    <div class="relative max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+      <div class="md:ml-auto md:w-1/2 md:pl-10">
+        <h2 class="text-lg font-semibold text-gray-300">Award winning support</h2>
+        <p class="mt-2 text-white text-3xl font-bold tracking-tight sm:text-4xl sm:tracking-tight">We’re here to help</p>
+        <p class="mt-3 text-lg text-gray-300">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat quisque ut interdum tincidunt duis.</p>
+        <div class="mt-8">
+          <div class="inline-flex rounded-md shadow">
+            <a href="#" class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-gray-900 bg-white hover:bg-gray-50">
+              Visit the help center
+              <!-- Heroicon name: solid/external-link -->
+              <svg class="-mr-1 ml-3 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                <path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z" />
+                <path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
