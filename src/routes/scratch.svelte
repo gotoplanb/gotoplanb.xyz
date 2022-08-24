a09434d67487e33c8ae6de33d3a04351ceab2041
Dave Stanton <dstanton@Daves-iMac-2.local>
2022-08-24 14:27:41 -0400

    A scratch route for playing.

---
+++ src/routes/scratch.svelte
@@ -0,0 +1,15 @@
+scratch
+<div class="grid grid-cols-2 md:grid-cols-12">
+	<div>01</div>
+	<div>02</div>
+	<div>03</div>
+	<div>04</div>
+	<div>05</div>
+	<div>06</div>
+	<div>07</div>
+	<div>08</div>
+	<div>09</div>
+	<div>10</div>
+	<div>11</div>
+	<div>12</div>
+</div>
